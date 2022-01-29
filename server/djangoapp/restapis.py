32a0ec40efe78052c347b4b95c6060d77ebe1526
Bedil09 <bedilrasya@gmail.com>
2022-01-29 03:59:12 +0000

    update and changes in week 4

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -13,32 +13,29 @@ from ibm_watson.natural_language_understanding_v1 import Features, SentimentOpti
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 
-
-def get_request(url, **kwargs):
-    print(kwargs)
-    print("GET from {} ".format(url))
-
-    try:
-
-       # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                params=kwargs)
-    except:
-        # If any error occurs
-        print("Network exception occurred")
-    status_code = response.status_code
-    print("With status {} ".format(status_code))
-    json_data = json.loads(response.text)
-    return json_data
-
-
 def get_request(url, **kwargs):
     print(kwargs)
     print("GET from {} ".format(url))
     try:
+        if "api_key" in kwargs:
+            params=dict()
+            params["text"]=kwargs["text"]
+            params["version"]=kwargs["version"]
+            params["features"]=kwargs["features"]
+            params["return_analyzed_text"]=kwargs["return_analyzed_text"]
+
+            print("params: ",params)
+
+            response = requests.get(url,params=params,headers={'Content-Type': 'application/json'},
+                auth=HTTPBasicAuth('apikey',kwargs["api_key"]))
+
+            print("Response_apikey_Provided: ",response)
+
+        else:
+            print("No api_key passed to get request.")
         # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                params=kwargs)
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
     except:
         # If any error occurs
         print("Network exception occurred")
@@ -99,54 +96,6 @@ def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
 
-
-def get_dealer_reviews_id_from_cf(url, **kwargs):
-    results = []
-    parameters = kwargs['kwargs']
-
-    json_result = get_request(url, **parameters)
-
-    if json_result:
-        reviews = json_result["body"]["data"]["docs"]
-        for review in reviews:
-            review_obj = DealerReview(
-                dealership=review["dealership"],
-                name=review["name"],
-                purchase=review["purchase"],
-                review=review["review"],
-            )
-
-            if "id" in review:
-                review_obj.id = review["id"]
-            if "purchase_date" in review:
-                review_obj.purchase_date = review["purchase_date"]
-            if "car_make" in review:
-                review_obj.car_make = review["car_make"]
-            if "car_model" in review:
-                review_obj.car_model = review["car_model"]
-            if "car_year" in review:
-                review_obj.car_year = review["car_year"]
-
-            if "review_time" in review:
-                review_obj.review_time = review["review_time"]
-
-            # sentiment="default"
-            print("Sentiment: ", review_obj.sentiment)
-            # review_obj.sentiment=analyze_review_sentiments(review_obj.review)
-            sentiment_response = analyze_review_sentiments(review_obj.review)
-            print("Sentiment_(NLU_RAW): ", sentiment_response)
-            if "sentiment" in sentiment_response:
-                # Extract Sentiment Label
-                review_obj.sentiment = sentiment_response['sentiment']['document']['label']
-            else:
-                review_obj.sentiment = sentiment_response['error']
-            print("Sentiment_(NLU_label_extract): ", review_obj.sentiment)
-            results.append(review_obj)
-            # print(results)
-
-    return results
-
-
 def get_dealer_reviews_by_id_from_cf(url, **kwargs):
     results = []
     json_result = get_request(url, **kwargs)
@@ -177,11 +126,11 @@ def get_dealer_reviews_by_id_from_cf(url, **kwargs):
 # - Get the returned sentiment label such as Positive or
 
 
-def analyze_review_sentiments(review):
+def analyze_review_sentiments(text):
     url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/379ef73c-0fb0-4834-968b-1eaeeadaf2f6/v1/analyze"
     api_key = "_LaQy6Ntzh18nqT4BK9i65TRshL-M6pnaC-2yKu05W9Z"
 
-    text = review
+    review=text
     authenticator = IAMAuthenticator(api_key)
     natural_language_understanding = NaturalLanguageUnderstandingV1(
         version='2021-12-28',
